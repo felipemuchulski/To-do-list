fad8bcf07bc6c88953268f8f94d631310335f73c
Felipe Muchulski de Andrades <lipemuchulski@gmail.com>
2022-04-20 16:39:40 -0300

    Criado primeira função de adicionar item

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
 'use strict';
 
-const criarItem = () => {
+/*<label class="todo__item">
+                <input type="checkbox">
+                <p>Adicionando item</p>
+                <input type="image" img class="img" src="../img/trash.png" />
+            </label>*/
+
+const criarItem = (text) => {
     const item = document.createElement('label')
-    item.classList.add()
+    item.classList.add('todo__item');
+    item.innerHTML = `<input type="checkbox">
+    <p>${text}</p>
+    <input type="image" img class="img" src="../img/trash.png" />
+    `
+
+    document.getElementById('todoList').appendChild(item)
 }
