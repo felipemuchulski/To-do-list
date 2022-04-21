8b740eb937f377a482e5aa57a4bbca02a57695ef
Felipe Muchulski de Andrades <lipemuchulski@gmail.com>
2022-04-20 23:52:20 -0300

    Alterando objeto

--- app.js
+++ app.js
@@ -1,18 +1,29 @@
 'use strict';
 
+let banco = [
+    {'tarefa' : 'Estudar JS'},
+    {'tarefa' : 'Netflix'},
+
+]
 /*<label class="todo__item">
                 <input type="checkbox">
                 <p>Adicionando item</p>
                 <input type="image" img class="img" src="../img/trash.png" />
             </label>*/
 
-const criarItem = (text) => {
+const criarItem = (tarefa) => {
     const item = document.createElement('label')
     item.classList.add('todo__item');
     item.innerHTML = `<input type="checkbox">
-    <p>${text}</p>
+    <p>${tarefa}</p>
     <input type="image" img class="img" src="../img/trash.png" />
     `
 
     document.getElementById('todoList').appendChild(item)
 }
+
+const atualizarTela = () => {
+    banco.forEach(item => criarItem(item.tarefa));
+}
+
+atualizarTela();
