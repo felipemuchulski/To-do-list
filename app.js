d9b56c8aac11fe6321fdddb696b4246786593eb1
Felipe Muchulski de Andrades <lipemuchulski@gmail.com>
2022-04-26 13:28:01 -0300

    Adicionado funções de adicionar novas tarefas

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 'use strict';
 
 let banco = [
-    {'tarefa' : 'Estudar JS'},
-    {'tarefa' : 'Netflix'},
+    {'tarefa' : 'Estudar JS', 'status': ''},
+    {'tarefa' : 'Netflix', 'status': ''},
 
 ]
 /*<label class="todo__item">
@@ -11,19 +11,43 @@ let banco = [
                 <input type="image" img class="img" src="../img/trash.png" />
             </label>*/
 
-const criarItem = (tarefa) => {
+const criarItem = (tarefa, status, indice) => {
     const item = document.createElement('label')
     item.classList.add('todo__item');
-    item.innerHTML = `<input type="checkbox">
+    item.innerHTML = `<input type="checkbox" ${status} data-indice=${indice}>
     <p>${tarefa}</p>
-    <input type="image" img class="img" src="../img/trash.png" />
-    `
+    <input type="image" img class="img" src="../img/trash.png" data-indice=${indice}> `
 
     document.getElementById('todoList').appendChild(item)
 }
 
-const atualizarTela = () => {
-    banco.forEach(item => criarItem(item.tarefa));
+const limparTarefas = () => {
+    const todoList = document.getElementById('todoList');
+    while(todoList.firstChild) {
+        todoList.removeChild(todoList.lastChild);
+    }
 }
+
+const atualizarTela = () => {
+    limparTarefas ();
+    banco.forEach( (item, indice) => criarItem(item.tarefa, item.status, indice));
+}
+
+const inserirItem = (evento) => {
+    const tecla = evento.key;
+    const texto = evento.target.value;
+    if(tecla === 'Enter') {
+        banco.push({'tarefa': texto, 'status': ''},)
+        atualizarTela();
+        evento.target.value = '';
+    }
+}
+
+const clickItem = (evento) => {
+    const elemento = evento.target; 
+}
+
+document.getElementById('newItem').addEventListener('keypress', inserirItem);
+document.getElementById('todoList').addEventListener('click', clickItem);
 
 atualizarTela();
